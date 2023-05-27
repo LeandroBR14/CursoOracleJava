320bd748e43011c7c2195530eb11fa2263a89f20
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-26 22:01:40 -0300

    first commit

---
+++ JFOPratica2/Problema2.java
@@ -0,0 +1,16 @@
+package JFOPratica2;
+
+public class Problema2 {
+    public static void main(String[] args) {
+        // ÁRVORE
+        System.out.println("         ^");
+        System.out.println("       ^ | ^");
+        System.out.println("     ^   |   ^");
+        System.out.println("   ^     |     ^");
+        System.out.println("  ^      |      ^");
+        System.out.println("^ ^ ^ ^ ^|^ ^ ^ ^ ^");
+        System.out.println("       |||||");
+        System.out.println();
+    }
+    
+}
