6db2d79a7c8a5bdde0ced15a7b61cf2d1f99fda5
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-05-28 21:57:51 -0300

    Pratica4 finalizada

---
+++ JFOPratica4/ComputeMethods.java
@@ -0,0 +1,18 @@
+package JFOPratica4;
+
+public class ComputeMethods {
+    public double fToC(double degreesF) {
+        double degreesC = (degreesF - 32) * 5 / 9;
+        return degreesC;
+    }
+
+    public double hipotenusa(int a, int b) {
+        double c = Math.sqrt(a * a + b * b);
+        return c;
+    }
+
+    public int dado() {
+        int randomNumber = (int) (Math.random() * 6) + 1;
+        return randomNumber;
+    }
+}
